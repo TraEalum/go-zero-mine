30520024fcaf596179c9b296863e47526d76df24
z <>
2022-10-26 15:10:48 +0800

    rm print

--- tools/goctl/goctl.go
+++ tools/goctl/goctl.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/zeromicro/go-zero/core/load"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/tools/goctl/cmd"
-	"github.com/zeromicro/go-zero/tools/goctl/internal/version"
-	"log"
 )
 
 func main() {
-	fmt.Println("fmt-goctl -v:", version.GetGoctlVersion())
-	log.Println("log-goctl -v:", version.GetGoctlVersion())
+	//fmt.Println("fmt-goctl -v:", version.GetGoctlVersion())
+	//log.Println("log-goctl -v:", version.GetGoctlVersion())
 	logx.Disable()
 	load.Disable()
 	cmd.Execute()
