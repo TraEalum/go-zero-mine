e9cc55dca3a8ebe2be2035f0a4bdcc61f64f231c
matteo <matteo-gz@users.noreply.github.com>
2022-11-09 09:31:22 +0800

    db field to ptr

--- tools/goctl/model/sql/template/field.go
+++ tools/goctl/model/sql/template/field.go
@@ -1,8 +1,8 @@
 package template
 
 // Field defines a filed template for types
-const Field = `{{.name}} {{.type}} {{.tag}} {{if .hasComment}}// {{.comment}}{{end}}`
+const Field = `{{.name}} *{{.type}} {{.tag}} {{if .hasComment}}//  {{.comment}} // src:tools/goctl/model/sql/template/field.go {{end}}`
 
-const MarshalFields = `m.{{.name}} = p.{{.name}}`
+const MarshalFields = `m.{{.name}} = &p.{{.name}} // src:tools/goctl/model/sql/template/field.go`
 
-const UnmarshalFields = `p.{{.name}} = m.{{.name}}`
+const UnmarshalFields = `p.{{.name}} = *m.{{.name}} // src:tools/goctl/model/sql/template/field.go`
