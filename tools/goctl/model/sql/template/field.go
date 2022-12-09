1b41456cb04708cc1cccb60ad634658ee1e103e5
matteo <matteo-gz@users.noreply.github.com>
2022-12-09 16:53:40 +0800

    code

--- tools/goctl/model/sql/template/field.go
+++ tools/goctl/model/sql/template/field.go
@@ -1,7 +1,8 @@
 package template
 
 // Field defines a filed template for types
-const Field = `{{.name}} *{{.type}} {{.tag}} {{if .hasComment}}//  {{.comment}} // src:tools/goctl/model/sql/template/field.go {{end}}`
+const Field = `{{.name}} {{.type}} {{.tag}} {{if .hasComment}}// {{.comment}}{{end}}`
+const FieldPtr = `{{.name}} *{{.type}} {{.tag}} {{if .hasComment}}//  {{.comment}} // src:tools/goctl/model/sql/template/field.go {{end}}`
 
 const MarshalFields = `m.{{.name}} = &p.{{.name}} // src:tools/goctl/model/sql/template/field.go`
 
